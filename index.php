e1ec9a0ca51d0ecaf6e02c744d060afafc31bf8f
samochyjnr <102660519+samochyjnr@users.noreply.github.com>
2022-03-30 10:41:39 +0100

    new

---
+++ index.php
@@ -0,0 +1,405 @@
+<!doctype html>
+<html lang="en">
+  <head>
+
+     <link href="css/style.css" rel="stylesheet"> 
+<link href="javascript.js" rel="stylesheet">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+
+    <title>UDEME'S PORTFOLIO</title>
+  </head>
+  <body>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+
+    <section id="toppart">
+
+      <nav class="navbar navbar-expand-lg navbar-dark">
+        <a class="navbar-brand" href="#">
+          <?php echo "<img src='img/uddy.png' alt='' width='30' height='24' class='d-inline-block align-top imgtop' >";
+           ?>
+          <?php echo "AKPAINYANG UDEME"; ?>
+        </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <?php echo "<span class='navbar-toggler-icon'></span>"; ?>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#aboutme">ABOUT ME <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#myservices">MY SKILLS & SERVICES</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#recentprojects">PORTFOLIO</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#testimonials">TESTIMONIALS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#contactme">CONTACT ME</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container">
+    <div class="row">
+
+  <?php
+  echo "<div class='col'></div>";
+  ?>
+    <div class="col">
+
+    <?php
+    echo "<img src='img/uddy.png' alt='profilepic' class='profilepic'>";
+    ?>
+
+    <?php
+    echo "<h2>AKPAINYANG UDEME MFONOBONG</h2>";
+    ?>
+
+    <?php
+    echo " <h2 class='h2i'>{uddy}</h2> ";
+    ?>
+
+    <?php
+    echo " <h3> WEB DEVELOPER</h3> ";
+    ?>
+
+    <?php
+    echo " <h3 class='h3i'> FULL STACK</h3> ";
+    ?>
+
+    </div>
+
+    <?php
+echo "<div class='col'></div>";
+     ?>
+
+
+    </div>
+    </div>
+
+
+    </section>
+
+<section>
+
+ <?php
+
+echo "<h3 class='h2ii' id='aboutme'> ABOUT ME</h3>";
+
+ ?>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+            <?php
+            echo "<img src='img/uddy2.jpg' alt='profilepic' class='pic1'>";
+             ?>
+      </div>
+      <div class="col">
+
+        <?php
+echo "<br>";
+echo "<br>";
+echo "<br>";
+         ?>
+
+
+
+        <?php
+echo " <img class='giphy1' src='https://media.giphy.com/media/H19TbCm0XQTPW/giphy.gif' alt='...'>  ";
+echo "<p> Hi there, my name is Akpainyang Udeme Mfonobong, I'm a full stack web developer and this is a website i created as a portfolio page. kindly scroll through to view the various options you might like and contact me. Thank you. </p>";
+
+         ?>
+
+         <?php
+ echo " <img class='giphy1' src='https://media.giphy.com/media/iIGT8Y1rOYhBpdHh1C/giphy.gif' alt='...'> ";
+ echo "<p> Based on my experience and ongoing development in this field as a web developer, I have acquired the necessary skills and knowledge necessary to make your project a success. </p>";
+
+          ?>
+
+          <?php
+  echo " <img class='giphy1' src='https://media.giphy.com/media/Vf3ZKdillTMOOaOho0/giphy.gif' alt='...'> ";
+  echo " <p>The experience and skills i have gained so far have provided me with a very strong foundation in my career and thus enables me to develop fully as a full stack web developer.</p>";
+
+           ?>
+
+           <?php
+   echo " <img class='giphy1' src='https://media.giphy.com/media/Itbxi5E73eH2mZK97v/giphy.gif' alt='...'>  ";
+   echo " <p> My goal is to make your dream/ideal website a reality as much as i can. Contact me so we can start today.<p>";
+
+            ?>
+
+      </div>
+    </div>
+  </div>
+
+
+
+
+</section>
+
+<section id="toppart">
+  <?php
+  echo "<br>";
+   ?>
+
+   <?php
+   echo "<h3 class='h3iii' id='myservices'> MY SERVICES </h3>";
+    ?>
+
+
+<div class="container">
+<div class="row">
+  <div class="col-sm">
+    <div class="card" style="width: 18rem;">
+      <?php
+      echo "<img src='https://media.giphy.com/media/3IrouyJoqgzv5KTPfM/giphy.gif' class='card-img-top' alt='...'>";
+       ?>
+
+    <div class="card-body">
+
+      <?php
+      echo "<h5 class='card-title'>FRONT END</h5>";
+       echo "<p class='card-text'>  Hire me to work on the front end part of the website. what you'd like the viewers to see when they log onto your website.</p>";
+       echo "  <a href='#' class='btn btn-primary'>HIRE ME</a>";
+       ?>
+
+
+
+    </div>
+  </div>
+
+  <?php echo "<br>"; ?>
+
+  </div>
+
+  <div class="col-sm">
+    <div class="card" style="width: 18rem;">
+    <?php
+    echo " <img src='https://media.giphy.com/media/WFZvB7VIXBgiz3oDXE/giphy.gif' class='card-img-top' alt='...'> ";
+     ?>
+    <div class="card-body">
+
+      <?php
+      echo "<h5 class='card-title'>BACK END</h5>";
+      echo "<p class='card-text'>Hire me to work on the back end of your website to be fully functional and tailored to your desire.<p>";
+      echo " <a href='#' class='btn btn-primary'>HIRE ME</a>";
+      ?>
+
+
+
+
+    </div>
+  </div>
+<br>
+
+
+  </div>
+  <div class="col-sm">
+    <div class="card" style="width: 18rem;">
+  <?php
+  echo "<img src='https://media.giphy.com/media/cIn5fTcjnKhStIeAef/giphy.gif' class='card-img-top' alt='..'>";
+   ?>
+  <div class="card-body">
+    <?php
+    echo "<h5 class='card-title'>FULL STACK</h5>";
+    echo "<p class='card-text'>Hire me to fully develop and or maintain your desired website.</p>";
+    echo "<a href='#' class='btn btn-primary'>HIRE ME</a>";
+     ?>
+
+  </div>
+</div>
+
+<?php
+echo "<br>";
+ ?>
+
+  </div>
+</div>
+</div>
+
+<?php
+echo "<br>";
+ ?>
+
+</section>
+
+
+<section>
+
+<?php
+echo "<h3 class='h2ii' id='recentprojects' > MY RECENT PROJECTS </h3>";
+ ?>
+
+</section>
+
+<section id="toppart">
+<?php
+echo "<br>";
+ ?>
+
+<?php
+echo "<h3 id='testimonials'> TESTIMONIALS </h3>";
+ ?>
+
+<?php
+echo "<br>";
+ ?>
+
+</section>
+
+<section>
+
+  <?php
+  echo "<h3 class='h2ii' id='contactme'>CONTACT ME </h3>";
+   ?>
+
+
+<div class="container">
+<div class="row">
+  <div class="col-sm">
+    <div class="card" style="width: 18rem;">
+    <?php
+    echo "<img src='https://media.giphy.com/media/jU9PVpqUvR0aNc3nvX/giphy.gif' class='card-img-top' alt='...'>";
+     ?>
+    <div class="card-body">
+      <?php
+      echo "<h5 class='card-title'>WHATSAPP CONTACT</h5>";
+      echo "<p class='card-text'> </p>";
+      echo "<a href='#' class='btn btn-primary'>WHATSAPP</a>";
+       ?>
+
+    </div>
+  </div>
+
+  <?php
+  echo "<br>";
+   ?>
+
+  <div class="card" style="width: 18rem;">
+  <?php
+echo "<img src='https://media.giphy.com/media/xpUtJjsEFZk7EcwPLs/giphy.gif' class='card-img-top'>";
+   ?>
+  <div class="card-body">
+    <?php
+    echo "<h5 class='card-title'>FACEBOOK</h5>";
+    echo "<p class='card-text'></p>";
+    echo "<a href='https://web.facebook.com/udeme.akpainyang/' class='btn btn-primary'>MY PROFILE</a>";
+     ?>
+
+
+
+  </div>
+</div>
+  </div>
+
+  <div class="col-sm">
+    <div class="card" style="width: 18rem;">
+      <?php
+    echo "<img src='https://media.giphy.com/media/HSUjurTAHO8ulVKgAP/giphy.gif' class='card-img-top' alt='...'>";
+       ?>
+    <div class="card-body">
+      <?php
+      echo "<h5 class='card-title'>SMS MESSAGE</h5>";
+      echo "<p class='card-text'><p>";
+      echo "<a href='#' class='btn btn-primary'>TEXT ME</a>";
+       ?>
+
+
+
+    </div>
+  </div>
+  <?php
+echo "<br>";
+   ?>
+
+  <div class="card" style="width: 18rem;">
+    <?php
+  echo "<img src='https://media.giphy.com/media/KnP4MW4dkeNJWCThSg/giphy.gif' class='card-img-top' alt='...'>";
+     ?>
+
+  <div class="card-body">
+    <?php
+    echo "<h5 class='card-title'>INSTAGRAM</h5>";
+    echo "<p class='card-text'></p>";
+    echo "<a href='https://www.instagram.com/uddy_7/' class='btn btn-primary'>MY PROFILE</a>";
+     ?>
+
+  </div>
+</div>
+
+  </div>
+  <div class="col-sm">
+    <div class="card" style="width: 18rem;">
+      <?php
+    echo "<img src='https://media.giphy.com/media/gbGf8mmc7L80bpwQcd/giphy.gif' class='card-img-top' alt='...'>";
+       ?>
+  <div class="card-body">
+    <?php
+    echo "<h5 class='card-title'>PHONE</h5>";
+    echo "<p class='card-text'></p>";
+    echo "<a href='08146358461' class='btn btn-primary'>CALL ME</a>";
+     ?>
+
+
+  </div>
+</div>
+<?php
+echo "<br>";
+ ?>
+
+<div class="card" style="width: 18rem;">
+  <?php
+echo "<img src='https://media.giphy.com/media/kH1DBkPNyZPOk0BxrM/giphy.gif' class='card-img-top' alt='...'>";
+   ?>
+  <div class="card-body">
+
+    <?php
+    echo "<h5 class='card-title'>GIT HUB</h5>";
+    echo "<p class='card-text'></p>";
+    echo "<a href='#' class='btn btn-primary'>MY PROFILE</a>";
+     ?>
+
+  </div>
+</div>
+  </div>
+</div>
+</div>
+
+</section>
+
+<section id="toppart">
+  <?php
+  echo "<br>";
+  echo "<br>";
+  echo "<br>";
+   ?>
+
+   <?php
+echo "<h6> © 2022 Akpainyang Udeme Mfonobong {Uddy} | All Rights Reserved </h6>";
+    ?>
+
+
+
+ <?php
+ echo "<br>";
+ echo "<br>";
+  ?>
+
+</section>
+
+
+
+  </body>
+</html>
---
+++ javascript.js
@@ -0,0 +1,103 @@
+
+
+
+  var a = document.getElementsByClassName("imglogo");
+  for(var elem in a){
+  a[elem].style.marginTop= "200px";
+}
+
+var a = document.getElementsByClassName("nav-link");
+for(var elem in a){
+a[elem].style.fontWeight= "bold";
+a[elem].style.marginTop= "10px";
+}
+
+var a = document.getElementsByClassName("profilepic");
+for(var elem in a){
+a[elem].style.width= "270px";
+a[elem].style.height= "270px";
+a[elem].style.marginTop= "70px";
+a[elem].style.marginLeft= "50px";
+}
+
+var a = document.getElementsByClassName("h2i");
+for(var elem in a){
+a[elem].style.textAlign= "center";
+a[elem].style.fontWeight= "bold";
+a[elem].style.fontSize= "23px";
+a[elem].style.marginTop= "10px";
+a[elem].style.color= "#E9EEF3";
+}
+
+var a = document.getElementsByClassName("h3i");
+for(var elem in a){
+a[elem].style.textAlign= "center";
+a[elem].style.fontWeight= "bold";
+a[elem].style.fontSize= "19px";
+a[elem].style.marginTop= "13px";
+a[elem].style.marginBottom= "120px";
+a[elem].style.color= "#E9EEF3";
+}
+
+var a = document.getElementsByClassName("h2ii");
+for(var elem in a){
+a[elem].style.color= "#000000";
+a[elem].style.fontWeight= "bold";
+a[elem].style.marginTop= "40px";
+a[elem].style.marginBottom= "20px";
+}
+
+var a = document.getElementsByClassName("pic1");
+for(var elem in a){
+a[elem].style.width= "450px";
+a[elem].style.height= "600px";
+a[elem].style.marginBottom= "20px";
+}
+
+var a = document.getElementsByClassName("giphy1");
+for(var elem in a){
+a[elem].style.width= "50px";
+a[elem].style.height= "50px";
+a[elem].style.float= "left";
+}
+
+var a = document.getElementsByClassName("h3iii");
+for(var elem in a){
+a[elem].style.fontWeight= "bold";
+a[elem].style.marginTop= "10px";
+a[elem].style.marginBottom= "10px";
+}
+
+var a = document.getElementsByClassName("card-img");
+for(var elem in a){
+a[elem].style.width= "20%";
+
+}
+var a = document.getElementsByClassName("card-bottom");
+for(var elem in a){
+a[elem].style.width= "20%";
+
+}
+var a = document.getElementsByClassName("card-img-top");
+for(var elem in a){
+a[elem].style.width= "20%";
+
+}
+
+var a = document.getElementsByClassName("card-title");
+for(var elem in a){
+a[elem].style.textAlign= "center";
+
+}
+
+var a = document.getElementsByClassName("btn");
+for(var elem in a){
+a[elem].style.alignContent= "center";
+
+}
+
+var a = document.getElementsByClassName("container");
+for(var elem in a){
+a[elem].style.alignItems= "center";
+
+}
